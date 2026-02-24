b04583f52c170112ab9e82113811039fe4bf32a4
Bảo Kha <baokha0305@gmail.com>
2026-02-24 15:10:55 +0700

    Support PostgreSQL and MySQL dynamically based on connection string URL

--- server/models/index.js
+++ server/models/index.js
@@ -1,22 +1,47 @@
 const { Sequelize } = require("sequelize");
 const config = process.env;
 
-const sequelize =
-  config.MYSQL_URL || config.DATABASE_URL
-    ? new Sequelize(config.MYSQL_URL || config.DATABASE_URL, {
-        dialect: "mysql",
-        logging: false,
-      })
-    : new Sequelize(
-        config.DB_NAME || config.MYSQLDATABASE || "QLThuVien",
-        config.DB_USER || config.MYSQLUSER || "root",
-        config.DB_PASS || config.MYSQLPASSWORD || "",
-        {
-          host: config.DB_HOST || config.MYSQLHOST || "localhost",
-          port: config.DB_PORT || config.MYSQLPORT || 3306,
-          dialect: "mysql",
-          logging: false,
-        },
-      );
+const url =
+  config.DATABASE_URL || config.MYSQL_URL || config.MYSQLURL || config.PG_URL;
+let connectionOptions = {};
+
+// Handle direct URL connections from Railway and external providers
+if (url) {
+  const isPostgres = url.startsWith("postgres");
+  connectionOptions = {
+    dialect: isPostgres ? "postgres" : "mysql",
+    ...(isPostgres ? { dialectModule: require("pg") } : {}),
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  };
+} else {
+  // Local/Direct environment variables
+  connectionOptions = {
+    host: config.DB_HOST || config.MYSQLHOST || "localhost",
+    port:
+      config.DB_PORT ||
+      config.MYSQLPORT ||
+      (config.MYSQLDATABASE ? 3306 : 5432),
+    dialect: config.DB_DIALECT || "mysql",
+    logging: false,
+  };
+}
+
+const sequelize = url
+  ? new Sequelize(url, connectionOptions)
+  : new Sequelize(
+      config.DB_NAME ||
+        config.MYSQLDATABASE ||
+        config.PGDATABASE ||
+        "QLThuVien",
+      config.DB_USER || config.MYSQLUSER || config.PGUSER || "root",
+      config.DB_PASS || config.MYSQLPASSWORD || config.PGPASSWORD || "",
+      connectionOptions,
+    );
 
 module.exports = sequelize;
