74d071492c55a2a5ac564a472855d6e65f049e0e
Bảo Kha <baokha0305@gmail.com>
2026-02-24 14:27:01 +0700

    Support Railway default MySQL environment variables

--- server/models/index.js
+++ server/models/index.js
@@ -2,14 +2,15 @@ const { Sequelize } = require("sequelize");
 const config = process.env;
 
 const sequelize = new Sequelize(
-  config.DB_NAME || "QLThuVien",
-  config.DB_USER || "root",
-  config.DB_PASS || "",
+  config.DB_NAME || config.MYSQLDATABASE || "QLThuVien",
+  config.DB_USER || config.MYSQLUSER || "root",
+  config.DB_PASS || config.MYSQLPASSWORD || "",
   {
-    host: config.DB_HOST || "localhost",
+    host: config.DB_HOST || config.MYSQLHOST || "localhost",
+    port: config.DB_PORT || config.MYSQLPORT || 3306,
     dialect: "mysql",
     logging: false,
-  }
+  },
 );
 
 module.exports = sequelize;
